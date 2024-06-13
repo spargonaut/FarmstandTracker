f113a8808d09d32c544c206de8a1ecb40f666ecd
spargonaut <1243607+spargonaut@users.noreply.github.com>
2024-06-12 21:28:55 -0700

    add database configuration

--- src/main/kotlin/com/farmstandtracker/plugins/Databases.kt
+++ src/main/kotlin/com/farmstandtracker/plugins/Databases.kt
@@ -1,13 +1,12 @@
 package com.farmstandtracker.plugins
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.sql.*
-import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.*
 
 fun Application.configureDatabases() {
+    Database.connect(
+        "jdbc:postgresql://localhost:5432/farmstand",
+        user = "postgres",
+        password = "mysecretpassword",
+    )
 }
