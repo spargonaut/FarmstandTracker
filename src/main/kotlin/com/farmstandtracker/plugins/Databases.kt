ac8fa791ccb78d245293a358496c1923aa22ca9d
spargonaut <1243607+spargonaut@users.noreply.github.com>
2024-10-10 19:55:04 -0700

    extract DB credentials to use environment variables

--- src/main/kotlin/com/farmstandtracker/plugins/Databases.kt
+++ src/main/kotlin/com/farmstandtracker/plugins/Databases.kt
@@ -6,7 +6,7 @@ import org.jetbrains.exposed.sql.*
 fun Application.configureDatabases() {
     Database.connect(
         "jdbc:postgresql://localhost:5432/farmstand_tracker",
-        user = "postgres",
-        password = "mysecretpassword",
+        user = System.getenv("FS_DB_USER") ?: "farmer",
+        password = System.getenv("FS_DB_PASSWORD") ?: "mysecretpassword",
     )
 }
